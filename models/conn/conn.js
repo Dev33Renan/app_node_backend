82cba2dda95a383d9c0b25ab1a4fa7a248b272eb
Dev33Renan <85537259+Dev33Renan@users.noreply.github.com>
2021-09-06 20:30:22 -0300

    mudanças

--- models/conn/conn.js
+++ models/conn/conn.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
-function Conn(url,porta,banco){
-    mongoose.connect(`mongodb+srv://dbAdmin:Isk45IOSFvuTWfIX@nodedb.6h7mj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+function Conn(url, user, pass, banco){
+    mongoose.connect(`${url}/${banco}`, {
+        user: user,
+        pass: pass,
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(() => { 
